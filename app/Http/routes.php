63600e8451258f1406591baf0f262405c96fc0d3
Jabib Leiton <jbleitonp@est.utn.ac.cr>
2016-06-26 15:40:32 -0600

    function downloadVideo and updateVideo Created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,12 +34,13 @@ Route::group(['middleware' => 'auth'], function () {
   Route::get('/video', 'VideoController@index');
   // download video
   Route::post('/video', 'VideoController@index');
-  Route::get('/download/{location}', function($location)
-  {
-    $file = $location;
-    return Response::download($file,'video.mp4',
-    ['Content-Type','application/mp4']);
-  });  //Route::post('/download/{location}', 'VideoController@download');
+  // Route::get('/download/{location}', function($location)
+  // {
+  //   $file = $location;
+  //   return Response::download($file,'video.mp4',
+  //   ['Content-Type','application/mp4']);
+  // });  //Route::post('/download/{location}', 'VideoController@download');
+  Route::post('/download/', 'VideoController@download');
 
 });
 Route::auth();
