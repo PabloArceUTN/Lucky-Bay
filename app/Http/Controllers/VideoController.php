3fa75e3dfb70025bd2c674d701a55742c8d6424e
Jabib Leiton <jbleitonp@est.utn.ac.cr>
2016-06-26 01:09:34 -0600

    json message to rabbitmq

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Video;
-use App\User;   
+use App\User;
 use App\Http\Requests;
 require '../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -27,12 +27,12 @@ class VideoController extends Controller
       $video->video_url=$url;
       $video->state="pending";
       $video->save();
-      echo $video->id;
-  // putQueue url video
+
+      $arr =json_encode(array('id'=>$video->id,'user_id'=>$user_id,'url'=>$url));
       $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
       $channel = $connection->channel();
       $channel->queue_declare('hello', false, false, false, false);
-      $msg = new AMQPMessage($url);
+      $msg = new AMQPMessage($arr);
       $channel->basic_publish($msg, '', 'hello');
       echo " [x] Sent 'URL!'\n";
       $channel->close();
